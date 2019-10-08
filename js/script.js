6f1c1db9b4c69cbbd1c94c9d74b61a52be0ec90b
Jose Vargas Traconis <josevargastr@gmail.com>
2019-10-08 15:05:06 -0500

    Updated styles to be used wit nihl

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
-$(function(){
+$(function () {
 	/* sert au preview sur la page main*/
-	$('form input').each(function(){
+	$('form input').each(function () {
 		var name = $(this).attr('name');
-		$(this).keyup(function(){
+		$(this).keyup(function () {
 			var val = $(this).val();
-			if(name == 'tel' && val!==''){
-				val = '<b style="color:#3896D6;font-size:14px;">Tel:&nbsp;</b>'+val;
+			if (name == 'tel' && val !== '') {
+				val = val;
 			}
-			if(name == 'puesto' && val!==''){
-				val = ''+val;
+			if (name == 'puesto' && val !== '') {
+				val = '' + val;
 			}
-			if(name == 'mail' && val!==''){
-				val = '<a style="margin-left:10px;color:#42484c;font-size:14px;" href="#"><b style="color:#3896D6;font-size:14px;">E:&nbsp;</b><span style="color:#41474c;font-size:14px;">'+val+'</span></a>';
+			if (name == 'mail' && val !== '') {
+				val = val;
 			}
-			
-			if(val==""){
-				if(name == 'tel' && val==''){
-					val = '<b style="color:#3896D6;font-size:14px;">Tel:&nbsp;</b>';
-				}
-				if(name == 'mail' && val==''){
-					val = '<b style="color:#3896D6;margin-left:10px;font-size:14px;">E:&nbsp;</b>';
-				}else if(name == 'pre' && val==''){
+
+			if (val == "") {
+				if (name == 'tel' && val == '') {
 					val = '';
 				}
-				else if(name == 'poste' && val==''){
+				if (name == 'mail' && val == '') {
 					val = '';
-				}else{
+				} else if (name == 'pre' && val == '') {
+					val = '';
+				}
+				else if (name == 'poste' && val == '') {
+					val = '';
+				} else {
 					val = $(this).attr("placeholder");
 				}
 			}
-			$('#preview .field-'+name).html( val );
+			$('#preview .field-' + name).html(val);
 			// update();
 		});
 	});
-	
-	
-	function update(){
-		$('form input').each(function(){
+
+
+	function update() {
+		$('form input').each(function () {
 			var name = $(this).attr('name');
-			$('#preview .field-'+name).html( $(this).val() );
+			$('#preview .field-' + name).html($(this).val());
 		});
 	}
-	
+
 });
